9388b25a4b8bd55b3d2107076420a3e00d5340a7
sohan110111 <sohan110111@gmail.com>
2020-06-18 22:49:17 +0600

    Java Comments

---
+++ Comments.java
@@ -0,0 +1,10 @@
+class Comments {
+    public static void main(String[] args) {
+        // This is a single line comment 
+        System.out.println("Java Comments"); // This is a comment
+        /* This is a multiline comments.
+         ............
+         .........
+         */
+    }
+}
