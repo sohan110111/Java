52f9ad5f69593bce930a810a9e0fa251d3a31517
sohan110111 <sohan110111@gmail.com>
2020-06-27 01:19:04 +0600

    Variable discussion

---
+++ Variable.java
@@ -0,0 +1,25 @@
+// syntax: type variable_name = value;
+
+public class Variable {
+    public static void main(String[] args) {
+        String name = "My Name is : Sohan";
+        System.out.println(name);
+
+        int myRollNumber = 18;
+        System.out.println("My roll number is : " + myRollNumber);
+
+        int age;
+        age = 20;
+        System.out.println("My age is : " + age);
+
+        int myMoney = 50000;
+        myMoney = 10000;
+        System.out.println("My present money is : " + myMoney);
+
+        final int myId = 1937820118;
+        System.out.println("My id is : " + myId);
+
+        const int myBirthDate = 16;
+        System.out.println("My birthday is : " + myBirthDate + "November");
+    }
+}
