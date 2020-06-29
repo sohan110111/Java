6ffc49a929f7e5bf5e00f2c61378b74d39bee7a5
sohan110111 <sohan110111@gmail.com>
2020-06-30 01:03:18 +0600

    Strings discussion

--- Strings.java
+++ Strings.java
@@ -1,37 +1,38 @@
 public class Strings {
     public static void main(String[] args) {
         String greeting = "Hello world";
-        System.out.println(greeting);
+        System.out.println(greeting); // output = Hello world
 
         String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        System.out.println("The length of the text string is : " + text.length());
-        System.out.println(greeting.toUpperCase());
-        System.out.println(greeting.toLowerCase());
+        System.out.println("The length of the text string is : " + text.length()); // output = The length of the text string is : 26
+        System.out.println(greeting.toUpperCase()); // output = HELLO WORLD
+        System.out.println(greeting.toLowerCase()); // output = hello world
 
         String textPosition = "Please locate where 'locate' occurs!";
-        System.out.println(textPosition.indexOf("locate"));
+        System.out.println(textPosition.indexOf("locate")); // output = 7
 
         String firstName = "John", lastName = "Doe";
-        System.out.println(firstName.concat(lastName));
+        System.out.println(firstName.concat(lastName)); // output = JohnDoe
         
         // here print Single quote and double quote 
         String doubleQuote = "Hello \"Bangladesh\"";
-        System.out.println(doubleQuote);
-        String singleQuote = "I'm alright"; // and here no need to use \'
-        System.out.println(singleQuote);
+        System.out.println(doubleQuote); // output = Hello "Bangladesh"
+        String singleQuote = "I'm alright"; // and here no need to use (\')
+        System.out.println(singleQuote); // output = I'm alright
         String backslash = "The character is called \\ backslash";
-        System.out.println(backslash);
+        System.out.println(backslash); // output = The character is called \ backslash
 
         String NewLine = "Hello \n USA";
-        System.out.println(NewLine);
+        System.out.println(NewLine); /* output = Hello
+                                                  USA */
         String CarriageReturn = "Hello\rWorld";
-        System.out.println(CarriageReturn);
+        System.out.println(CarriageReturn); // output = World
         String Tab = "Hello\tWorld";
-        System.out.println(Tab);
-        String Backspace = "Hel\blo world";
-        System.out.println(Backspace);
+        System.out.println(Tab); // output = Hello   World
+        String Backspace = "Hel\blo world"; 
+        System.out.println(Backspace); // output =  Helo world
         String FromFeed = "Hello\fworld"; // Page breaking ASCII control character
-        System.out.println(FromFeed);
+        System.out.println(FromFeed); // output =  Hello♀world
 
         // Numbers are added by plus(+) and String are concatenate 
         int x = 10, y = 3, z = x + y;
