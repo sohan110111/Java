b2253e9e21938993bc47a7192c4da3584f2feb3e
sohan110111 <sohan110111@gmail.com>
2020-06-30 01:59:10 +0600

    Java Boolean partial discussion

---
+++ Booleans.java
@@ -0,0 +1,11 @@
+public class Booleans {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        System.out.println(isJavaFun); // Output result shows = true
+        boolean isJavaNotScientific = false;
+        System.out.println(isJavaNotScientific); // Output result shows = false
+
+        // Here boolean expression 
+        System.out.println(5 > 7); // Output is false because 5 is not greater than 7
+    }
+}
